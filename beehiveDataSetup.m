b3267a9b452d506d2ab1fc1d2cfa0fc0ef078f79
Trevor Vannoy <trevorvannoy@montana.edu>
2023-06-05 15:26:02 -0600

    update data path for use on tempest

--- beehiveDataSetup.m
+++ beehiveDataSetup.m
@@ -1,5 +1,5 @@
-baseDir = '/Volumes/ece-bmw-lab/insect-lidar/projects/mlsp2023/beehives';
-rawDataDir = '/Volumes/ece-bmw-lab/insect-lidar/msu-bee-hives';
+baseDir = '../data';
+rawDataDir = [baseDir filesep 'raw'];
 combinedDataDir = [baseDir filesep 'combined'];
 preprocessedDataDir = [baseDir filesep 'preprocessed'];
 trainingDir = [baseDir filesep 'training'];
