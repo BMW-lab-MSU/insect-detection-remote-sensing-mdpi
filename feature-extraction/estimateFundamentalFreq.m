73acf47412bebfa579923ce850cde214c315581e
Trevor Vannoy <trevorvannoy@montana.edu>
2023-06-14 13:18:20 -0600

    [wip] optimize by replacing findpeaks with islocalmax
    islocalmax appears to be ~11 times faster than findpeaks when not
    using a parallel pool. When using a parallel pool of 8 workers for
    findpeaks, islocalmax is ~1.89 times faster. Note that islocalmax is
    always single-threaded. islocalmax could be parallelized as well if I
    split the data up into nWorkers separate sections, which should
    achieve a near-linear speedup, I think. Just some things to think about.
    
    I will revisit this optimization later.

--- feature-extraction/estimateFundamentalFreq.m
+++ feature-extraction/estimateFundamentalFreq.m
@@ -13,24 +13,42 @@ arguments
     opts.UseParallel (1,1) logical = false
 end
 
-fundamental = zeros(height(psd), 1);
 
+
+% TODO: nHarmonics should be an optional input parameter
 hps = harmonicProductSpectrum(psd, 3);
 
-if opts.UseParallel
-    nWorkers = gcp('nocreate').NumWorkers;
-else
-    nWorkers = 0;
-end
+% if opts.UseParallel
+%     nWorkers = gcp('nocreate').NumWorkers;
+% else
+%     nWorkers = 0;
+% end
 
-parfor (i = 1:height(psd), nWorkers)
-    [~, fundamentalTmp] = findpeaks(hps(i,:), 'NPeaks', 1, ...
-        'SortStr', 'descend');
-    
-    % if we don't find a fundamental frequency, then we keep it set at 0.
-    if ~isempty(fundamentalTmp)
-        fundamental(i) = fundamentalTmp;
-    end
-end
+
+% TODO: don't hardcode the optional parameters, or at least come up with
+% reasonable values
+% NOTE: islocalmax appears to be ~11 times faster than findpeaks when not
+% using a parallel pool. When using a parallel pool of 8 workers for
+% findpeaks, islocalmax is ~1.89 times faster. Note that islocalmax is
+% always single-threaded. islocalmax could be parallelized as well if I
+% split the data up into nWorkers separate sections, which should achieve a
+% near-linear speedup, I think. Just some things to think about.
+[maximaIndicator] = islocalmax(hps,2,"FlatSelection","center","MaxNumExtrema",1,"MinProminence",1,"MinSeparation",5);
+
+[row, fundamentalLocTmp] = find(maximaIndicator);
+
+[~,sortIdx] = sort(row);
+
+fundamental = fundamentalLocTmp(sortIdx);
+
+% parfor (i = 1:height(psd), nWorkers)
+%     [~, fundamentalTmp] = findpeaks(hps(i,:), 'NPeaks', 1, ...
+%         'SortStr', 'descend');
+% 
+%     % if we don't find a fundamental frequency, then we keep it set at 0.
+%     if ~isempty(fundamentalTmp)
+%         fundamental(i) = fundamentalTmp;
+%     end
+% end
 
 end
