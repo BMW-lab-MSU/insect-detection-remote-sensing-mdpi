cc2cdc6b8a860a209355d8a09ff0f74501f9fbca
Trevor Vannoy <trevorvannoy@montana.edu>
2023-06-23 15:52:35 -0600

    add sparse coding paths

--- pathSetup.m
+++ pathSetup.m
@@ -1,2 +1,4 @@
 %% Path setup
-addpath('feature-extraction','figures','lib','preprocessing','training','testing')
+addpath('feature-extraction','figures','lib','preprocessing','training','testing','sparse-coding-bees')
+addpath('/home/group/bradwhitaker/sparse-coding-toolboxes/omp')
+addpath('/home/group/bradwhitaker/sparse-coding-toolboxes/ksvd')
