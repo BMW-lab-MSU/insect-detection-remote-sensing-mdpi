0589cc15531abf25387b5a5d3266a436b5306ff5
Trevor Vannoy <trevorvannoy@montana.edu>
2023-06-23 12:13:48 -0600

    convert data to single-precision to save RAM

--- preprocessing/combineData.m
+++ preprocessing/combineData.m
@@ -42,7 +42,9 @@ for index = 1:length(dates)
             imageNum = imageNum + 1;
 
 
-            data{imageNum} = adjusted_data_junecal(scanNum).data;
+	    % conver data to single-precision to save RAM
+            data{imageNum} = single(adjusted_data_junecal(scanNum).data);
+
             rowLabels(imageNum) = labels.rowLabels(scanNum);
             imgLabels(imageNum) = labels.imageLabels(scanNum); 
 
