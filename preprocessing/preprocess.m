2927ba5c05c52f0bce5c67900e76fb3bc795a6e2
Trevor Vannoy <trevorvannoy@montana.edu>
2023-06-27 16:45:57 -0600

    reduce pmt ringing by thresholding

---
+++ preprocessing/preprocess.m
@@ -0,0 +1,27 @@
+% SPDX-License-Identifier: BSD-3-Clause
+
+%% Load in data
+filenames = ["2022-06-combined", "2022-07-combined"];
+
+for filename = filenames
+    load(combinedDataDir + filesep + filename)
+end
+
+%% Threshold data to reduce PMT ringing
+% PMT rigning manifests as data > 0 volts, so we can threshold all positive values to 0.
+% This doesn't reduce all ringing artifacts, but it helps.
+% NOTE: this could be turned into a function later if we do more preprocessing and 
+%       want to make this more modular.
+juneData(juneData > 0) = 0;
+julyData(julyData > 0) = 0;
+
+%% Save data
+if ~exist(preprocessedDataDir)
+    mkdir(baseDataDir, "preprocessed");
+end
+save(preprocessedDataDir + filesep + "2022-06-preprocessed.mat", ...
+    'juneData', 'juneRowLabels', 'juneImgLabels', ...
+    'juneMetadata', '-v7.3');
+save(preprocessedDataDir + filesep + "2022-07-preprocessed.mat", ...
+    'julyData', 'julyRowLabels', 'julyImgLabels', ...
+    'julyMetadata', '-v7.3');
