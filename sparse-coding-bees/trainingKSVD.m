f47817a6f11a88144be708e6944d542dcb8479c8
Trevor Vannoy <trevorvannoy@montana.edu>
2023-06-26 21:15:36 -0600

    update data directory names

--- sparse-coding-bees/trainingKSVD.m
+++ sparse-coding-bees/trainingKSVD.m
@@ -7,7 +7,7 @@ nIter = 25;
 memUsage = 'high';
 
 % 10% Non-bee training data
-load(trainingDir + filesep + "ksvdTrainingData.mat");
+load(trainingDataDir + filesep + "ksvdTrainingData.mat");
 
 % convert training data to a matrix instead of cell array;
 % the resulting matrix needs dimensions of 1024 x (# of observations),
@@ -16,9 +16,9 @@ data = vertcat(ksvdTrainingData{:}).';
 
 [D,err] = trainKSVD(data,numSparse,dSize,nIter,memUsage);
 
-if ~exist(baseDir + filesep + "sparse-coding")
-    mkdir(baseDir, "sparse-coding");
+if ~exist(sparseCodingDataDir)
+    mkdir(baseDataDir, "sparse-coding");
 end
 
-save(baseDir + filesep + "sparse-coding" + filesep + "D" + string(dSize) + ".mat",'D', '-v7.3');
-save(baseDir + filesep + "sparse-coding" + filesep + "err" + string(dSize) + ".mat",'err', '-v7.3');
+save(sparseCodingDataDir + filesep + "D" + string(dSize) + ".mat",'D', '-v7.3');
+save(sparseCodingDataDir + filesep + "err" + string(dSize) + ".mat",'err', '-v7.3');
