b80f03919b0ea241d346699eec8868cab8e27733
Trevor Vannoy <trevorvannoy@montana.edu>
2023-06-26 21:03:22 -0600

    cleanup comments and fix missing filesep

--- sparse-coding-bees/trainingKSVD.m
+++ sparse-coding-bees/trainingKSVD.m
@@ -3,18 +3,22 @@ rng(0,"twister");
 % Global Training and Reconstruction Variables
 numSparse = 4;
 dSize = 2048;
+nIter = 25;
+memUsage = 'high';
 
-% 10% Non-Bee Training
+% 10% Non-bee training data
 load(trainingDir + filesep + "ksvdTrainingData.mat");
 
-% convert training data to a matrix instead of cell array
+% convert training data to a matrix instead of cell array;
+% the resulting matrix needs dimensions of 1024 x (# of observations),
+% which is transposed compared to how our data is originally stored.
 data = vertcat(ksvdTrainingData{:}).';
 
-[D,err] = trainKSVD(data,numSparse,dSize,200,'high');
+[D,err] = trainKSVD(data,numSparse,dSize,nIter,memUsage);
 
-if ~exist(baseDir + "sparse-coding")
+if ~exist(baseDir + filesep + "sparse-coding")
     mkdir(baseDir, "sparse-coding");
 end
 
-save(baseDir + filsep + "sparse-coding" + filsep + "D" + string(dSize) + ".mat",'D', '-v7.3');
-save(baseDir + filsep + "sparse-coding" + filsep + "err" + string(dSize) + ".mat",'err', '-v7.3');
+save(baseDir + filesep + "sparse-coding" + filesep + "D" + string(dSize) + ".mat",'D', '-v7.3');
+save(baseDir + filesep + "sparse-coding" + filesep + "err" + string(dSize) + ".mat",'err', '-v7.3');
