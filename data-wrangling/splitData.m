4f7456b42a4e4f2fe20a5130f072838faa1945ca
Trevor Vannoy <trevorvannoy@montana.edu>
2023-06-27 20:10:48 -0600

    update where data is loaded from
    Now we are loading the combined preprocessed data, since we perform
    preprocessing before splitting the data.

--- data-wrangling/splitData.m
+++ data-wrangling/splitData.m
@@ -3,43 +3,36 @@
 % Set random number generator properties for reproducibility
 rng(0, 'twister');
 
-FOLDER_PREFIX = "MSU-horticulture-farm-bees-";
+%% Load data
+% The June days are being used as the training/validation set and the July days
+% are used as the testing set. This emulates a scenario where you collect data,
+% train, then test the models during the next data collection campaign.
 
-%% Combine training data
-% The training data is the data taken in June: 2022-06-23 and 2022-06-24
-trainingDays = ["2022-06-23", "2022-06-24"];
-trainingFolderTimestamps0623 = ["122126" "135615" "141253" "144154" "145241"];
-trainingFolderTimestamps0624 = ["094752" "095001" "104012" "105017" "110409" "111746" "113017" "114343" "115816"];
-trainingFolderTimestamps = {trainingFolderTimestamps0623, trainingFolderTimestamps0624};
+% load training data
+load(preprocessedDataDir + filesep + "2022-06-preprocessed")
 
-[trainValData,trainValRowLabels,trainValImgLabels,trainValMeta] = combineData(trainingDays, ...
-    FOLDER_PREFIX, trainingFolderTimestamps, rawDataDir);
-
-%% Combine testing data
-% The testing data is the data taken in July: 2022-07-28 and 2022-07-29
-testingDays = ["2022-07-28", "2022-07-29"];
-testingFolderTimestamps0728 = ["112652" "120850" "123948" "124905" "131133" "133834" "135906" "141427" "143013" "144821"];
-testingFolderTimestamps0729 = ["093945" "095958" "101924"];
-testingFolderTimestamps = {testingFolderTimestamps0728, testingFolderTimestamps0729};
-
-[testingData,testingRowLabels,testingImgLabels,testingMetadata] = combineData(testingDays, ...
-    FOLDER_PREFIX, testingFolderTimestamps, rawDataDir);
-
+% load testing data
+load(preprocessedDataDir + filesep + "2022-07-preprocessed")
 
 %% Partition training data into train and validations sets
-
 VALIDATION_PCT = 0.2;
 
-holdoutPartition = cvpartition(trainValImgLabels, 'Holdout', VALIDATION_PCT, 'Stratify', true);
+holdoutPartition = cvpartition(juneImgLabels, 'Holdout', VALIDATION_PCT, 'Stratify', true);
 
-trainingData = trainValData(training(holdoutPartition));
-validationData = trainValData(test(holdoutPartition));
-trainingImgLabels = trainValImgLabels(training(holdoutPartition));
-validationImgLabels = trainValImgLabels(test(holdoutPartition));
-trainingRowLabels = trainValRowLabels(training(holdoutPartition));
-validationRowLabels = trainValRowLabels(test(holdoutPartition));
-trainingMetadata = trainValMeta(training(holdoutPartition));
-validationMetadata = trainValMeta(test(holdoutPartition));
+trainingData = juneData(training(holdoutPartition));
+validationData = juneData(test(holdoutPartition));
+trainingImgLabels = juneImgLabels(training(holdoutPartition));
+validationImgLabels = juneImgLabels(test(holdoutPartition));
+trainingRowLabels = juneRowLabels(training(holdoutPartition));
+validationRowLabels = juneRowLabels(test(holdoutPartition));
+trainingMetadata = juneMetadata(training(holdoutPartition));
+validationMetadata = juneMetadata(test(holdoutPartition));
+
+%% Rename testing data variables
+testingData = julyData;
+testingImgLabels = julyImgLabels;
+testingRowLabels = julyRowLabels;
+testingMetadata = julyMetadata;
 
 
 %% Save training and testing data
@@ -60,6 +53,7 @@ save(trainingDataDir + filesep + "trainingDataNoSparseCoding.mat", ...
 if ~exist(validationDataDir)
     mkdir(baseDataDir, "validation");
 end
-save(validationDataDir + filesep + "valiationDataNoSparseCoding.mat", ...
+save(validationDataDir + filesep + "validationDataNoSparseCoding.mat", ...
     'validationData', 'validationRowLabels', 'validationImgLabels', ...
     'validationMetadata', 'holdoutPartition', '-v7.3');
+
