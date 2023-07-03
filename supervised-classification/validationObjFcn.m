92ceb3eae07c48dc2ad0916d34b318436e9a5a03
Trevor Vannoy <trevorvannoy@montana.edu>
2023-07-03 10:49:41 -0600

    update validation function to work with bayesopt
    validationObjFcn now takes in optional static and optimizable
    parameters, and uses those to construct the classifier with the correct
    hyperparameters.

---
+++ supervised-classification/validationObjFcn.m
@@ -0,0 +1,61 @@
+function [objective,constraints,userdata] = ...
+    validationObjFcn(classifierConstructor,...
+    trainingData,trainingLabels,validationData,validationLabels,params,opts)
+% SPDX-License-Identifier: BSD-3-Clause
+arguments
+    classifierConstructor function_handle
+    trainingData 
+    trainingLabels
+    validationData 
+    validationLabels
+    params.Static = {}
+    params.Optimizable = []
+    opts.UseParallel (1,1) logical = false
+end
+
+statset('UseParallel', opts.UseParallel);
+
+% Assmeble the classifier's hyperparameter arguments
+classifierArgs = {};
+
+if ~isempty(params.Static)
+    classifierArgs = horzcat(classifierArgs, namedargs2cell(params.Static));
+end
+
+if ~isempty(params.Optimizable)
+    classifierArgs = horzcat(classifierArgs, ...
+        namedargs2cell(table2struct(params.Optimizable)));
+end
+
+% Construct classifier
+if isempty(classifierArgs)
+    % use default hyperapameters
+    classifier = classifierConstructor();
+else
+    classifier = classifierConstructor(classifierArgs{:});
+end
+
+% Train the model
+fit(classifier,trainingData,trainingLabels);
+
+% Predict labels on the validation set
+predLabels = predict(classifier,validationData);
+
+% Compute performance metrics
+confusionMatrix = confusionmat(validationLabels, predLabels);
+
+[accuracy, precision, recall, f2, f3, mcc] = analyzeConfusion(confusionMatrix);
+objective = -mcc;
+
+constraints = [];
+
+userdata.Confusion = confusionMatrix;
+userdata.Classifier = classifier;
+userdata.Accuracy = accuracy;
+userdata.Precision = precision;
+userdata.Recall = recall;
+userdata.F2 = f2;
+userdata.F3 = f3;
+userdata.MCC = mcc;
+userdata.PredLabels = predLabels;
+end
