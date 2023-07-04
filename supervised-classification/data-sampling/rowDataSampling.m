f2703b75ad16725cf3fb9415c5cc7b7157162888
Trevor Vannoy <trevorvannoy@montana.edu>
2023-07-04 10:49:18 -0600

    put under/oversampling into a standalone function
    This should reduce code duplication

---
+++ supervised-classification/data-sampling/rowDataSampling.m
@@ -0,0 +1,46 @@
+function [newData,newLabels,newFeatures] = rowDataSampling(undersampleRatio,...
+    nOversample,data,labels,features,opts)
+
+arguments
+    undersampleRatio
+    nOversample
+    data
+    labels
+    features = []
+    opts.MajorityLabel = 0
+    opts.UseParallel = false
+end
+
+if opts.UseParallel
+    nWorkers = gcp('nocreate').NumWorkers;
+else
+    nWorkers = 0;
+end
+
+newData = data;
+newLabels = labels;
+
+% Undersample by removing rows from the majority class
+parfor(imageNum = 1:numel(data), nWorkers)
+    idxRemove = randomUndersample(labels,opts.MajorityLabel,
+        UndersamplingRatio=undersampleRatio,Reproducible=true);
+    newData{imageNum}(idxRemove) = [];
+    newLabels{imageNum}(idxRemove) = [];
+end
+
+% Oversampling
+if isempty(features)
+    % Create synthesic insect time series rows
+    [synthData,synthLabels] = createSyntheticData(newData,newLabels,...
+        nOversample,UseParallel=opts.UseParallel);
+
+        newData = vertcat(newData,synthFeatures);
+        newLabels = vertcat(newLabels,synthLabels);
+else
+    % Create synthetic insect features
+    [synthFeatures,synthLabels] = createSyntheticFeatures(newData,newLabels,...
+        nOversample,UseParallel=opts.UseParallel);
+    
+        newFeatures = vertcat(features,synthFeatures);
+        newLabels = vertcat(newLabels,synthLabels);
+end
