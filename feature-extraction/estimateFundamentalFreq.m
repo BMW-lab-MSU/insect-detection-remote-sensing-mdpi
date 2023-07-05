8ed04d185ce39ef01c39cd497f20f7421ca2a9ed
Trevor Vannoy <trevorvannoy@montana.edu>
2023-07-05 12:27:27 -0600

    cleanup unused code

--- feature-extraction/estimateFundamentalFreq.m
+++ feature-extraction/estimateFundamentalFreq.m
@@ -1,4 +1,4 @@
-function fundamental = estimateFundamentalFreq(psd, opts)
+function fundamental = estimateFundamentalFreq(psd)
 % estimateFundamentalFreq estimate the fundamental frequency in a PSD using the
 % harmonic product spectrum
 %
@@ -10,29 +10,12 @@ function fundamental = estimateFundamentalFreq(psd, opts)
 % SPDX-License-Identifier: BSD-3-Clause
 arguments
     psd (:,:) {mustBeNumeric}
-    opts.UseParallel (1,1) logical = false
 end
-
-
 
 % TODO: nHarmonics should be an optional input parameter
 hps = harmonicProductSpectrum(psd, 3);
 
-% if opts.UseParallel
-%     nWorkers = gcp('nocreate').NumWorkers;
-% else
-%     nWorkers = 0;
-% end
 
-
-% TODO: don't hardcode the optional parameters, or at least come up with
-% reasonable values
-% NOTE: islocalmax appears to be ~11 times faster than findpeaks when not
-% using a parallel pool. When using a parallel pool of 8 workers for
-% findpeaks, islocalmax is ~1.89 times faster. Note that islocalmax is
-% always single-threaded. islocalmax could be parallelized as well if I
-% split the data up into nWorkers separate sections, which should achieve a
-% near-linear speedup, I think. Just some things to think about.
 [maximaIndicator] = islocalmax(hps,2,"FlatSelection","center","MaxNumExtrema",1);
 
 [row, fundamentalLocTmp] = find(maximaIndicator);
@@ -40,15 +23,5 @@ hps = harmonicProductSpectrum(psd, 3);
 [~,sortIdx] = sort(row);
 
 fundamental = fundamentalLocTmp(sortIdx);
-
-% parfor (i = 1:height(psd), nWorkers)
-%     [~, fundamentalTmp] = findpeaks(hps(i,:), 'NPeaks', 1, ...
-%         'SortStr', 'descend');
-% 
-%     % if we don't find a fundamental frequency, then we keep it set at 0.
-%     if ~isempty(fundamentalTmp)
-%         fundamental(i) = fundamentalTmp;
-%     end
-% end
 
 end
