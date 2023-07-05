57abf3a6ae9dd21715f86af76b6a2e8d0c80be88
Trevor Vannoy <trevorvannoy@montana.edu>
2023-07-05 11:26:28 -0600

    remove sparse coding
    Sparse coding doesn't appear to be doing what we want it to, and it
    didn't help the changepoint detection results

--- beehiveDataSetup.m
+++ beehiveDataSetup.m
@@ -6,11 +6,9 @@ preprocessedDataDir = baseDataDir + filesep + "preprocessed";
 trainingDataDir = baseDataDir + filesep + "training";
 testingDataDir = baseDataDir + filesep + "testing";
 validationDataDir = baseDataDir + filesep + "validation";
-sparseCodingDataDir = baseDataDir +  filesep + "sparse-coding";
 
 % TODO: constants for filenames?, e.g. TRAINING_DATA = "trainingData.mat"
 
 %% Results directories
 baseResultsDir = "../results";
-sparseCodingResultsDir = baseResultsDir + filesep + "sparse-coding";
 
