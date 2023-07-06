400660cfdb29f239cff9eb62ba6eb1318863bb8c
Trevor Vannoy <trevorvannoy@montana.edu>
2023-07-05 21:19:25 -0600

    [fix] return empty values from function when no insects are present

--- preprocessing/createSyntheticData.m
+++ preprocessing/createSyntheticData.m
@@ -19,6 +19,8 @@ insectIdx = find(labels == 1);
 % Return early is there aren't any insects in the data;
 % we can't create synthetic insects if no insects are available :)
 if isempty(insectIdx)
+    synthData = [];
+    synthLabels = logical.empty;
     return
 end
 
