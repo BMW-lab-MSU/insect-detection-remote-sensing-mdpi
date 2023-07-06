fa41bcbb26c44fb556a36605d8cea4c40a31053d
Trevor Vannoy <trevorvannoy@montana.edu>
2023-07-05 19:26:37 -0600

    add subfolders to path and change to strings

--- pathSetup.m
+++ pathSetup.m
@@ -1,6 +1,8 @@
 %% Path setup
-addpath('feature-extraction')
-addpath('lib')
-addpath('preprocessing')
-addpath('data-wrangling')
-addpath('supervised-classification')
+addpath("feature-extraction")
+addpath("lib")
+addpath("preprocessing")
+addpath("data-wrangling")
+addpath("supervised-classification")
+addpath("supervised-classification/classifiers")
+addpath("supervised-classification/data-sampling")
