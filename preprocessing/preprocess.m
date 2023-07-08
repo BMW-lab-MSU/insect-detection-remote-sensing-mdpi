5c4f02a0eb2bafcaea94c02579d48445f5022753
Trevor Vannoy <trevorvannoy@montana.edu>
2023-07-08 16:39:36 -0600

    remove filtering
    Neither bandpass nor highpass filtering improved the changepoint
    detection results, so we're removing it. The filtering probably
    altered the time-domain characteristics too much, which the algorithms
    rely on.

--- preprocessing/preprocess.m
+++ preprocessing/preprocess.m
@@ -34,39 +34,3 @@ save(preprocessedDataDir + filesep + "2022-07-preprocessed.mat", ...
     'julyData', 'julyRowLabels', 'julyImgLabels', ...
     'julyMetadata', '-v7.3');
 
-%% Highpass filter the data
-% TODO: udpate comment after this works or doesn't...
-% The passband is set to 50--1200 Hz. Honeybee wingbeat frequencies are 
-% typically between 100 and 500 Hz, usually on the lower end of the range.
-% We want to pass at least the 3rd harmonic for the bees, so the upper
-% passband frequency is set to 1200 Hz, which is hte 3rd harmonic for 400 Hz.
-% The bee signals we've looked at have fundamentals between 100--200 Hz.
-%
-% We use a Butterworth filter so we have a maximally flat passband.
-cutoff = 50;
-filterOrder = 4;
-
-parfor i = 1:numel(juneData)
-    % Compute the average PRF for the image so we can scale the passband
-    % frequencies into normalized frequencies.
-    fs = averagePRF(juneMetadata(i).Timestamps);
-
-    juneData{i} = highpassFilter(juneData{i},filterOrder,cutoff,fs);
-end
-
-parfor i = 1:numel(julyData)
-    fs = averagePRF(julyMetadata(i).Timestamps);
-
-    julyData{i} = highpassFilter(julyData{i},filterOrder,cutoff,fs);
-end
-
-%% Save data
-if ~exist(preprocessedDataDir)
-    mkdir(baseDataDir, "preprocessed");
-end
-save(preprocessedDataDir + filesep + "2022-06-preprocessed-filtered.mat", ...
-    'juneData', 'juneRowLabels', 'juneImgLabels', ...
-    'juneMetadata', '-v7.3');
-save(preprocessedDataDir + filesep + "2022-07-preprocessed-filtered.mat", ...
-    'julyData', 'julyRowLabels', 'julyImgLabels', ...
-    'julyMetadata', '-v7.3');
