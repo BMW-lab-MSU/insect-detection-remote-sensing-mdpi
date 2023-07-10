fa84374e6e18d1bfcddb8bbf8447e088dbfd7546
Trevor Vannoy <trevorvannoy@montana.edu>
2023-07-10 10:11:27 -0600

    compute average PRF for each image

---
+++ dataset-analysis/computePRFStats.m
@@ -0,0 +1,12 @@
+%% Load in meatadata
+
+load(combinedDataDir + filesep + "2022-06-combined", "juneMetadata")
+load(combinedDataDir + filesep + "2022-07-combined", "julyMetadata")
+
+%% Combine metadata into one variable for easier analysis
+
+metadata = horzcat(juneMetadata,julyMetadata);
+
+%% compute the average PRF for each image
+
+meanPRF = averagePRF(vertcat(metadata.Timestamps));
