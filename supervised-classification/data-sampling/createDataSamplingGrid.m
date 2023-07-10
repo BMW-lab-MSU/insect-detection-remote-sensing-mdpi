2c13d6d1c39cb160792b802c5025c6e79b4b6664
Trevor Vannoy <trevorvannoy@montana.edu>
2023-07-10 15:37:19 -0600

    reduce undersampling grid search space
    We now have 16 instead of 20 grid points to search. This will speed
    things up.

--- supervised-classification/data-sampling/createDataSamplingGrid.m
+++ supervised-classification/data-sampling/createDataSamplingGrid.m
@@ -1,7 +1,7 @@
 % SPDX-License-Identifier: BSD-3-Clause
 
 % Setup undersampling and augmentation ranges
-params.UndersamplingRatio = [0 0.2 0.4 0.6 0.8];
+params.UndersamplingRatio = [0 0.25 0.5 0.75];
 params.NSyntheticInsect = round([0,logspace(0,log10(100),3)]);
 
 samplingGrid = formatGridSearchParams(params);
