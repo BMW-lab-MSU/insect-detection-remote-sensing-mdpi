a00fb9dd1163f6e6fcad461bead438dedfa69264
Trevor Vannoy <trevorvannoy@montana.edu>
2023-07-14 20:50:10 -0700

    add support for different parameters in each convolution layer

--- supervised-classification/classifiers/CNN1d.m
+++ supervised-classification/classifiers/CNN1d.m
@@ -12,7 +12,6 @@ classdef CNN1d < DeepLearning1dClassifier
 
     methods (Static)
         function params = getDefaultParameters()
-            params.NLayers=2;
             params.FilterSize=20;
             params.NFilters=20;
             params.DropoutProbability=0.2;
@@ -24,13 +23,99 @@ classdef CNN1d < DeepLearning1dClassifier
             params.InitialLearnRate=0.01;
             params.MiniBatchSize=2048;
         end
+
+        function formattedParams = formatOptimizableParams(optimizableParams)
+            % bayesopt using tables instead of structs, but we need to use
+            % structs so we can convert Name-Value pairs into a cell array
+            % that we can pass to the classifier constructor
+            formattedParams = table2struct(optimizableParams);
+
+            fieldNames = fields(formattedParams);
+
+            nFilterSizeParams = nnz(contains(fieldNames,"FilterSize"))
+            nFiltersParams = nnz(contains(fieldNames,"NFilters"));
+
+            % Determine how many layers the network will have. Either
+            % 1. NFilters and FilterSize have the same number of elements,
+            % in which case the number of layers is the number of elements.
+            % Each element of NFilters and FilterSize specifies the respective
+            % paramaters for each layer.
+            % 2. NFilters has 1 element, and FilterSize has multiple. The 
+            % number of layers is the number of FilterSize arguments. Each
+            % layer has the same number of filters.
+            % 3. FilterSize has 1 element, and NFilters has multiple. The
+            % number of layers is the number of NFilters arguments. Each
+            % layer has the same filter size.
+            if nFiltersParams == nFilterSizeParams
+                nLayers = nFiltersParams;
+
+                filterSize = zeros(1,nLayers);
+                nFilters = zeros(1,nLayers);
+
+                % Create the FilterSize and NFilter arrays
+                for layerNum = 1:nLayers
+                    filterSize(layerNum) = formattedParams.("FilterSize" + layerNum);
+                    nFilters(layerNum) = formattedParams.("NFilters" + layerNum);
+
+                    % Remove individual FilterSize and NFilters field since
+                    % they are combined into arrays
+                    formattedParams = rmfield(formattedParams,"FilterSize" + layerNum);
+                    formattedParams = rmfield(formattedParams,"NFilters" + layerNum);
+                end
+
+            elseif nFiltersParams == 1
+                nLayers = nFilterSizeParams;
+
+                filterSize = zeros(1,nLayers);
+                nFilters = zeros(1,1);
+
+                nFiltersFieldName = fieldNames(find(contains(fieldNames,"NFilters")));
+
+                % Create the FilterSize and NFilter arrays
+                for layerNum = 1:nLayers
+                    filterSize(layerNum) = formattedParams.("FilterSize" + layerNum);
+                    nFilters(layerNum) = formattedParams.(nFiltersFieldName);
+
+                    % Remove individual FilterSize fields
+                    formattedParams = rmfield(formattedParams,"FilterSize" + layerNum);
+                end
+
+                formattedParams = rmfield(formattedParams,nFiltersFieldName);
+
+            elseif nFilterSizeParams == 1
+                nLayers = nFiltersParams;
+
+                filterSize = zeros(1,1);
+                nFilters = zeros(1,nLayers);
+
+                filterSizeFieldName = fieldNames(find(contains(fieldNames,"FilterSize")));
+
+                % Create the FilterSize and NFilter arrays
+                for layerNum = 1:nLayers
+                    nFilters(layerNum) = formattedParams.("NFilters" + layerNum);
+                    filterSize(layerNum) = formattedParams.(filterSizeFieldName);
+
+                    % Remove individual NFilters fields
+                    formattedParams = rmfield(formattedParams,"NFilters" + layerNum);
+                end
+
+                formattedParams = rmfield(formattedParams,filterSizeFieldName);
+
+            else
+                error("Number of FilterSize and NFilters parameters are ",...
+                    + "inconsistent and don't define a valid number of ",...
+                    + "layers. They must be the same size, or one of ",...
+                    + "them must have only 1 element.");
+            end
+
+            formattedParams.LayerSizes = layerSizes;
+        end
     end
 
     methods
 
         function obj = CNN1d(params,trainingOpts,opts)
             arguments
-                params.NLayers=2
                 params.FilterSize=20
                 params.NFilters=20
                 params.DropoutProbability=0.2
@@ -41,10 +126,18 @@ classdef CNN1d < DeepLearning1dClassifier
                 trainingOpts.MaxEpochs=5
                 trainingOpts.InitialLearnRate=0.01
                 trainingOpts.MiniBatchSize=2048
-                opts.UseGPU=true
+                opts.UseGPU=(true && canUseGPU())
             end
 
-            obj.Name = "CNN1d" + params.NLayers + "Layers";
+            nLayers = numel(params.FilterSize);
+
+            % Validate that NFilters is the same size as FilterSize. The number
+            % of layers is defined by the size of these arguments.
+            if numel(params.NFilters) ~= nLayers
+                error("NFilters and FilterSize must be the same size");
+            end
+
+            obj.Name = "CNN1d" + nLayers + "Layers";
 
             params = obj.createCostMatrix(params);
 
@@ -68,13 +161,14 @@ classdef CNN1d < DeepLearning1dClassifier
                 Plots="none",...
                 ExecutionEnvironment=executionEnvironment);
 
+            % Create the input layer
             layers = [sequenceInputLayer(1,MinLength=params.SequenceLength)];
 
-            % Add the convolution layers. Each layer is identical for now.
-            for layerNum = 1:params.NLayers
+            % Add the convolution layers.
+            for layerNum = 1:nLayers
                 layers = [...
                     layers,...
-                    convolution1dLayer(params.FilterSize,params.NFilters),...
+                    convolution1dLayer(params.FilterSize(layerNum),params.NFilters(layerNum)),...
                     batchNormalizationLayer,...
                     reluLayer,...
                     dropoutLayer(params.DropoutProbability),...
