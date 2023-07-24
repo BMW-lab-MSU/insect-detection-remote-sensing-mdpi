de5972764db79b03f51493a2991c0c6ef5ebcb87
Trevor Vannoy <trevorvannoy@montana.edu>
2023-07-24 09:57:33 -0600

    add function to train 2D CNN with default params
    We aren't performing a data sampling grid search for the 2D methods, so
    we need to train them with their default parameters in case the defaults
    perform better than the parameters bayesopt chooses.

---
+++ supervised-classification/cnn2d-default-training/trainCNN2dManualParams.m
@@ -0,0 +1,45 @@
+function trainCNN2dManualParams(classifierType,params,opts)
+
+arguments
+    classifierType function_handle
+    params.ClassifierParams = {}
+    opts.UseParallel = false
+    opts.UseGPU = false
+end
+
+if opts.UseParallel
+    if isempty(gcp('nocreate'))
+        parpool();
+    end
+end
+
+% Set up data paths
+beehiveDataSetup;
+
+% Load in the training data
+load(trainingDataDir + filesep + "trainingData","trainingData",...
+    "trainingImgLabels","trainingMetadata");
+
+% Load in the validation data
+load(validationDataDir + filesep + "validationData","validationData","validationImgLabels");
+
+
+% Train and evaluate the classifier with the given data sampling parameters
+[objective,~,userdata] = validationObjFcn(classifierType,trainingData,...
+    trainingImgLabels,validationData,validationImgLabels,...
+    UseParallel=opts.UseParallel,UseGPU=opts.UseGPU,...
+    Static=params.ClassifierParams);
+
+disp("objective = " + objective);
+
+% Save results so we can do parameter selection later
+filename = userdata.Classifier.Name + "ManualParamTraining.mat";
+
+if ~exist(trainingResultsDir + filesep + "default-params")
+    mkdir(trainingResultsDir,"default-params");
+end
+
+save(trainingResultsDir + filesep + "default-params" + filesep + filename,...
+    "objective","userdata",'-v7.3');
+
+end
