16e8306e537330f19352016fc2afdde4289909a0
Nathaniel Bennett Sweeney <natbsweeney@gmail.com>
2023-07-24 10:23:37 -0600

    Consolidated figures to one script

--- figures/createChangepointConfMatrices.m
+++ figures/createChangepointConfMatrices.m
@@ -4,18 +4,20 @@ load("../../results/changepoint-results/finalRowResults.mat");
 
 % Font Settings
 paperFontName = "Times New Roman";
-paperFontSize = 11;
+paperFontSize = 10;
+figSize = [18.46 7];
 
 % Figure 1 : Image Results
-fig1 = figure(1);
+fig1 = figure("Units","centimeters","Position",[1 1 figSize(1) figSize(2)]);
 t1 = tiledlayout(fig1,2,3,"TileSpacing","compact","Padding","tight");
-t1.Title.String = "Image Results"; t1.Title.FontName = paperFontName; t1.Title.FontSize;
+% t1.Title.String = "Image Results"; t1.Title.FontName = paperFontName; t1.Title.FontSize;
 t1.XLabel.String = "Predicted"; t1.XLabel.FontName = paperFontName; t1.XLabel.FontSize = paperFontSize;
 t1.YLabel.String = "True"; t1.YLabel.FontName = paperFontName; t1.YLabel.FontSize = paperFontSize;
 
     % gfpop Rows
     p1 = nexttile;
     c1 = confusionchart(finalImageResults{1,1},{'No Bee' 'Bee'});
+    c1.sortClasses(["No Bee","Bee"]);
     c1.Title = finalImageResults{2,1};
     c1.YLabel = ""; c1.XLabel = "";
     c1.FontName = paperFontName; c1.FontSize = paperFontSize;
@@ -23,6 +25,7 @@ t1.YLabel.String = "True"; t1.YLabel.FontName = paperFontName; t1.YLabel.FontSiz
     % gfpop Columns
     p2 = nexttile;
     c2 = confusionchart(finalImageResults{1,2},{'No Bee' 'Bee'});
+    c2.sortClasses(["No Bee","Bee"]);
     c2.Title = finalImageResults{2,2};
     c2.YLabel = ""; c2.XLabel = "";
     c2.FontName = paperFontName; c2.FontSize = paperFontSize;
@@ -30,6 +33,7 @@ t1.YLabel.String = "True"; t1.YLabel.FontName = paperFontName; t1.YLabel.FontSiz
     % gfpop Both
     p3 = nexttile;
     c3 = confusionchart(finalImageResults{1,3},{'No Bee' 'Bee'});
+    c3.sortClasses(["No Bee","Bee"]);
     c3.Title = finalImageResults{2,3};
     c3.YLabel = ""; c3.XLabel = "";
     c3.FontName = paperFontName; c3.FontSize = paperFontSize;
@@ -37,6 +41,7 @@ t1.YLabel.String = "True"; t1.YLabel.FontName = paperFontName; t1.YLabel.FontSiz
     % Matlab Rows
     p4 = nexttile;
     c4 = confusionchart(finalImageResults{1,4},{'No Bee' 'Bee'});
+    c4.sortClasses(["No Bee","Bee"]);
     c4.Title = finalImageResults{2,4};
     c4.YLabel = ""; c4.XLabel = "";
     c4.FontName = paperFontName; c4.FontSize = paperFontSize;
@@ -44,6 +49,7 @@ t1.YLabel.String = "True"; t1.YLabel.FontName = paperFontName; t1.YLabel.FontSiz
     % Matlab Columns
     p5 = nexttile;
     c5 = confusionchart(finalImageResults{1,5},{'No Bee' 'Bee'});
+    c5.sortClasses(["No Bee","Bee"]);
     c5.Title = finalImageResults{2,5};
     c5.YLabel = ""; c5.XLabel = "";
     c5.FontName = paperFontName; c5.FontSize = paperFontSize;
@@ -51,6 +57,7 @@ t1.YLabel.String = "True"; t1.YLabel.FontName = paperFontName; t1.YLabel.FontSiz
     % Matlab Both
     p6 = nexttile;
     c6 = confusionchart(finalImageResults{1,6},{'No Bee' 'Bee'});
+    c6.sortClasses(["No Bee","Bee"]);
     c6.Title = finalImageResults{2,6};
     c6.YLabel = ""; c6.XLabel = "";
     c6.FontName = paperFontName; c6.FontSize = paperFontSize;
@@ -58,15 +65,16 @@ t1.YLabel.String = "True"; t1.YLabel.FontName = paperFontName; t1.YLabel.FontSiz
     exportgraphics(fig1,"changepointImageMatrices.pdf","ContentType","vector");
 
 % Figure 2 : Row Results
-fig2 = figure(2);
+fig2 = figure("Units","centimeters","Position",[1 1 figSize(1) figSize(2)]);
 t2 = tiledlayout(fig2,2,3,"TileSpacing","compact","Padding","tight");
-t2.Title.String = "Row Results";
+% t2.Title.String = "Row Results";
 t2.XLabel.String = "Predicted";
 t2.YLabel.String = "True";
 
     % gfpop Rows
     p7 = nexttile;
     c7 = confusionchart(finalRowResults{1,1},{'No Bee' 'Bee'});
+    c7.sortClasses(["No Bee","Bee"]);
     c7.Title = finalRowResults{2,1};
     c7.YLabel = ""; c7.XLabel = "";
     c7.FontName = paperFontName; c7.FontSize = paperFontSize;
@@ -74,6 +82,7 @@ t2.YLabel.String = "True";
     % gfpop Columns
     p8 = nexttile;
     c8 = confusionchart(finalRowResults{1,2},{'No Bee' 'Bee'});
+    c8.sortClasses(["No Bee","Bee"]);
     c8.Title = finalRowResults{2,2};
     c8.YLabel = ""; c8.XLabel = "";
     c8.FontName = paperFontName; c8.FontSize = paperFontSize;
@@ -81,6 +90,7 @@ t2.YLabel.String = "True";
     % gfpop Both
     p9 = nexttile;
     c9 = confusionchart(finalRowResults{1,3},{'No Bee' 'Bee'});
+    c9.sortClasses(["No Bee","Bee"]);
     c9.Title = finalRowResults{2,3};
     c9.YLabel = ""; c9.XLabel = "";
     c9.FontName = paperFontName; c9.FontSize = paperFontSize;
@@ -88,6 +98,7 @@ t2.YLabel.String = "True";
     % Matlab Rows
     p10 = nexttile;
     c10 = confusionchart(finalRowResults{1,4},{'No Bee' 'Bee'});
+    c10.sortClasses(["No Bee","Bee"]);
     c10.Title = finalRowResults{2,4};
     c10.YLabel = ""; c10.XLabel = "";
     c10.FontName = paperFontName; c10.FontSize = paperFontSize;
@@ -95,6 +106,7 @@ t2.YLabel.String = "True";
     % Matlab Columns
     p11 = nexttile;
     c11 = confusionchart(finalRowResults{1,5},{'No Bee' 'Bee'});
+    c11.sortClasses(["No Bee","Bee"]);
     c11.Title = finalRowResults{2,5};
     c11.YLabel = ""; c11.XLabel = "";
     c11.FontName = paperFontName; c11.FontSize = paperFontSize;
@@ -102,6 +114,7 @@ t2.YLabel.String = "True";
     % Matlab Both
     p12 = nexttile;
     c12 = confusionchart(finalRowResults{1,6},{'No Bee' 'Bee'});
+    c12.sortClasses(["No Bee","Bee"]);
     c12.Title = finalRowResults{2,6};
     c12.YLabel = ""; c12.XLabel = "";
     c12.FontName = paperFontName; c12.FontSize = paperFontSize;
--- figures/createPaperGraphics.m
+++ figures/createPaperGraphics.m
@@ -2,38 +2,59 @@
 % Plot 1: Four examples of bees (body, wings, blips, hard target)
 % Plot 2: Transit and Frequency Histograms
 
+clear figures;
+
 % Example Bees Figures
 %   Body: 06/24-095001-193
 %   Wings: 06/24-095001-221
 %   Blip: 06/24-095001-154
 %   Hard Target: 06/24-111746-32
-clear;
-
-% Data Loading
-
 bodyImgNum = 193;
 wingImgNum = 221;
-blipImgNum = 154;
+blipImgNum = 105;
 hardImgNum = 32;
 
 load("../../data/raw/2022-06-24/MSU-horticulture-farm-bees-095001/adjusted_data_junecal_volts.mat");
 bodyImg = adjusted_data_junecal(bodyImgNum).data;
 wingImg = adjusted_data_junecal(wingImgNum).data;
+imgTime = adjusted_data_junecal(bodyImgNum).time;
+
+load("../../data/raw/2022-06-24/MSU-horticulture-farm-bees-104012/adjusted_data_junecal_volts.mat");
 blipImg = adjusted_data_junecal(blipImgNum).data;
-imgTime = adjusted_data_junecal(blipImgNum).time;
 
 load("../../data/raw/2022-06-24/MSU-horticulture-farm-bees-111746/adjusted_data_junecal_volts.mat");
 hardImg = adjusted_data_junecal(hardImgNum).data;
 clear adjusted_data_junecal;
-%%
+
+% Transit Time and Frequency Figures
+load("../../code/transit-frequency-analysis/harmonicRowCount.mat");
+load("../../code/transit-frequency-analysis/beeTransitTimes.mat")
+
+% Before and After Figures
+%   2022-06-combined-889
+beforeImgNum = 889;
+load("../../data/combined/2022-06-combined.mat","juneData");
+beforeImg = juneData{beforeImgNum};
+
+load("../../data/preprocessed/2022-06-preprocessed.mat","juneData");
+afterImg = juneData{beforeImgNum};
+
+clear juneData
+
+%% General Paper Settings
+
+% Font Settings
+imgFontName = 'Helvetica';
+imgFontSize = 12;
+figSizeSmall = [18.46 7];
+figSizeBig = [1 1];
+
+%% Figure 1 : Example Bee Images
+
 
 % Display Settings
 rowStop = 60;
 colStop = 1024;
-
-% Font Settings
-imgFontName = "Times New Roman";
-imgFontSize = 10;
 
 % Time Settings
 SEC_TO_MS = 1e3;
@@ -52,36 +73,30 @@ range = rangeStart:rangeResolution:rangeEnd;
 time = timeStart*1e3:timeEnd*1e3/1024:timeEnd*1e3;
 
 % Figure and Layout Settings
-fig1 = figure(1);
+fig1 = figure("Units","centimeters","Position",[1 1 figSizeSmall(1) figSizeSmall(2)]); hold on;
 t1 = tiledlayout(fig1,2,2,"TileSpacing","tight","Padding","compact");
-t1.XLabel.String = "Time [ms]";
-t1.XLabel.FontName = imgFontName;
-t1.XLabel.FontSize = imgFontSize;
-t1.YLabel.String = "Range [m]";
-t1.YLabel.FontName = imgFontName;
-t1.YLabel.FontSize = imgFontSize;
 
 % Body Settings
 p1 = nexttile;
-imagesc(time,range(1:rowStop),bodyImg(1:rowStop,1:colStop),[-.4 .1]); title("Body Example");
+imagesc(time,range(1:rowStop),bodyImg(1:rowStop,1:colStop),[-.2 .1]); title("(a)","FontWeight","bold");
 p1.FontName = imgFontName; p1.FontSize = imgFontSize;
 p1.XTickMode = "manual";
 
 % Wing Settings
 p2 = nexttile;
-imagesc(time,range(1:rowStop),wingImg(1:rowStop,1:colStop),[-.4 .1]); title("Wings Example");
+imagesc(time,range(1:rowStop),wingImg(1:rowStop,1:colStop),[-.2 .1]); title("(b)","FontWeight","bold");
 p2.FontName = imgFontName; p2.FontSize = imgFontSize;
 p2.XTickMode = "manual";
 p2.YTickMode = "manual";
 
 % Blip Settings
 p3 = nexttile;
-imagesc(time,range(1:rowStop),blipImg(1:rowStop,1:colStop),[-.4 .1]); title("Blip Example");
+imagesc(time,range(30:90),blipImg(30:90,1:colStop),[-.2 .1]); title("(c)","FontWeight","bold");
 p3.FontName = imgFontName; p3.FontSize = imgFontSize;
 
 % Hard Target Settings
 p4 = nexttile;
-imagesc(time,range(1:rowStop),hardImg(1:rowStop,1:colStop),[-.4 .1]); title("Hard Target w/ Bees");
+imagesc(time,range(1:rowStop),hardImg(1:rowStop,1:colStop),[-.2 .1]); title("(d)","FontWeight","bold");
 p4.FontName = imgFontName; p4.FontSize = imgFontSize;
 p4.YTickMode = "manual";
 
@@ -90,23 +105,43 @@ cbar = colorbar;
 cbar.Layout.Tile = 'east';
 cbar.YDir = 'reverse';
 cbar.Label.String = 'PMT voltage (V)';
-cbar.Label.FontSize = 11;
-cbar.Label.FontName = 'Times New Roman';
-cbar.FontSize = 10;
-cbar.FontName = 'Times New Roman';
+cbar.Label.FontSize = imgFontSize;
+cbar.Label.FontName = imgFontName;
+cbar.FontSize = imgFontSize;
+cbar.FontName = imgFontName;
+
+% Rectangle a
+annotation(fig1,'rectangle',[0.141187004564268 0.707500000000003 0.0620642831347056 0.0642559523809532],'Color',[1 0 0]);
+
+% Rectangle b
+annotation(fig1,'rectangle',[0.706466775009029 0.749166666666667 0.0620642831347055 0.0642559523809532],'Color',[1 0 0]);
+
+% Rectangle c1
+annotation(fig1,'rectangle',[0.353525598538445 0.389318181818185 0.0324141432119143 0.0642559523809532],'Color',[1 0 0]);
+
+% Rectangle c2
+annotation(fig1,'rectangle',[0.306179830963122 0.249166666666669 0.0352835836710251 0.0642559523809534],'Color',[1 0 0]);
+
+% Rectangle d1
+annotation(fig1,'rectangle',[0.494128181034857 0.374166666666669 0.0620642831347054 0.0642559523809532],'Color',[1 0 0]);
+
+% Rectangle d2
+annotation(fig1,'rectangle',[0.577341954349061 0.343863636363639 0.0620642831347054 0.0642559523809532],'Color',[1 0 0]);
 
 % Colormap
 cmap = colormap(brewermap([],'Blues'));
 colormap(flipud(cmap));
 
+t1.XLabel.String = "Time [ms]";
+t1.XLabel.FontName = imgFontName;
+t1.XLabel.FontSize = imgFontSize;
+t1.YLabel.String = "Range [m]";
+t1.YLabel.FontName = imgFontName;
+t1.YLabel.FontSize = imgFontSize;
+
 exportgraphics(fig1,"beeExamples.pdf","ContentType","vector");
 
-%%
-% Transit Time and Frequency Histograms
-
-% Load Data
-load("../../code/transit-frequency-analysis/harmonicRowCount.mat");
-load("../../code/transit-frequency-analysis/beeTransitTimes.mat")
+%% Figure 2 : Transit Time and Frequency Histograms
 
 % General Variables
 faceColor = [0 .5 1];
@@ -115,25 +150,79 @@ edgeColor = [0 .5 1];
 edgeAlpha = .15;
 
 % Figure and Layout Settings
-fig2 = figure(2); clf;
+fig2 = figure("Units","centimeters","Position",[1 1 figSizeSmall(1) figSizeSmall(2)]); clf; hold on;
 t2 = tiledlayout(fig2,1,2,"TileSpacing","tight","Padding","compact");
-t2.Title.String = "Transit Time and Fundamental Frequency Histogram"; t2.Title.FontName = imgFontName;
-t2.YLabel.String = "Count"; t2.YLabel.FontName = imgFontName; t2.YLabel.FontSize = imgFontSize;
 
 % Transit Time Settings
 p5 = nexttile; hold on;
 p5.FontName = imgFontName; p5.FontSize = imgFontSize; 
 p5.YLabel.String = "Bee Count";
 p5.XLabel.String = "Transit Time [ms]"; 
-p5.XLim = [0 .31];
-h1 = histogram(transitTimes); h1.EdgeColor = edgeColor; h1.FaceColor = faceColor; h1.EdgeAlpha = edgeAlpha; h1.FaceAlpha = faceAlpha;
+p5.Title.String = "(a)"; p5.Title.FontWeight = "bold";
+h1 = histogram(transitTimes.*1000); h1.EdgeColor = edgeColor; h1.FaceColor = faceColor; h1.EdgeAlpha = edgeAlpha; h1.FaceAlpha = faceAlpha;
+p5.XLim = [0 p5.XLim(2)];
 
 % Harmonics Settings
 p6 = nexttile; hold on;
 p6.FontName = imgFontName; p6.FontSize = imgFontSize;
 p6.YLabel.String = "Row Count";
 p6.XLabel.String = "Est. Fundamental Frequency [Hz]";
-p6.XLim = [0 901];
+p6.Title.String = "(b)"; p6.Title.FontWeight = "bold";
 h2 = histogram(cell2mat(harmonicRowCount)); h2.EdgeColor = edgeColor; h2.FaceColor = faceColor; h2.EdgeAlpha = edgeAlpha; h2.FaceAlpha = faceAlpha;
+p6.XLim = [0 p6.XLim(2)];
 
 exportgraphics(fig2,"transitFrequenciesHistograms.pdf","ContentType","vector");
+
+hold off;
+
+%% Figure 3 : Ringing thresholding example
+
+fig3 = figure("Units","centimeters",Position=[1 1 figSizeSmall(1) figSizeSmall(2)]); hold on;
+t3 = tiledlayout(fig3,1,2,"TileSpacing","tight","Padding","compact");
+t3.XLabel.String = "Time (ms)";
+t3.XLabel.FontName = imgFontName;
+t3.XLabel.FontSize = imgFontSize;
+t3.YLabel.String = "Range (m)";
+t3.YLabel.FontName = imgFontName;
+t3.YLabel.FontSize = imgFontSize;
+
+% Body Settings
+p7 = nexttile;
+imagesc(time,range(1:rowStop),beforeImg(1:rowStop,1:colStop),[-.2 .1]); 
+p7.FontName = imgFontName; p7.FontSize = imgFontSize;
+p7.Title.String = "(a)"; p7.Title.FontWeight = "bold";
+
+% Wing Settings
+p8 = nexttile;
+imagesc(time,range(1:rowStop),afterImg(1:rowStop,1:colStop),[-.2 .1]);
+p8.FontName = imgFontName; p8.FontSize = imgFontSize;
+p8.Title.String = "(b)"; p8.Title.FontWeight = "bold";
+
+% p2.XTickMode = "manual";
+p8.YTickMode = "manual";
+yticks([])
+
+
+% Colorbar
+cbar = colorbar;
+cbar.Layout.Tile = 'east';
+cbar.YDir = 'reverse';
+cbar.Label.String = 'PMT voltage (V)';
+cbar.Label.FontSize = imgFontSize;
+cbar.Label.FontName = imgFontName;
+cbar.FontSize = imgFontSize;
+cbar.FontName = imgFontName;
+
+% Colormap
+cmap = colormap(brewermap([],'Blues'));
+colormap(flipud(cmap));
+
+% Rectangle Annotations
+annotation(fig3,'rectangle',[0.0774748923959828 0.500000000000001 0.0459110473457679 0.0871212121212128],'Color',[1 0 0]);
+annotation(fig3,'rectangle',[0.316638450502152 0.522727272727273 0.0764748923959827 0.0871212121212126],'Color',[1 0 0]);
+annotation(fig3,'rectangle',[0.4743748923959828 0.500000000000001 0.0459110473457679 0.0871212121212128],'Color',[1 .5 0]);
+annotation(fig3,'rectangle',[0.716638450502152 0.522727272727273 0.0764748923959827 0.0871212121212126],'Color',[1 .5 0]);
+
+exportgraphics(fig3,"thresholdingExample.pdf","ContentType","vector");
+
+hold off;
