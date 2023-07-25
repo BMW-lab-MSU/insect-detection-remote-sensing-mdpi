2ca229f0edd9d8bbadc7449f612b45539bcc2d6d
Trevor Vannoy <trevorvannoy@montana.edu>
2023-07-25 17:02:33 -0600

    [fix] make true labels a cell array
    The true labels that are passed in are formatted as one logical vector
    for all images; they need to be split into one cell per image, with 178
    logical values in each cell.

--- lib/imageConfusion.m
+++ lib/imageConfusion.m
@@ -2,10 +2,13 @@ function [confmat,trueImageLabels,predImageLabels] = imageConfusion(pred, target
 
 % SPDX-License-Identifier: BSD-3-Clause
 
-predLabelsCell = mat2cell(pred, 178*ones(1,numel(pred)/178), 1);
+nImages = numel(pred)/178;
+
+predLabelsCell = mat2cell(pred, 178*ones(1,nImages), 1);
+trueLabelsCell = mat2cell(target, 178*ones(1,nImages),1);
 
 
 % get vectors of image labels
-trueImageLabels = cellfun(@(c) any(c), target);
+trueImageLabels = cellfun(@(c) any(c), trueLabelsCell);
 predImageLabels = cellfun(@(c) any(c), predLabelsCell);
 confmat = confusionmat(trueImageLabels, predImageLabels);
