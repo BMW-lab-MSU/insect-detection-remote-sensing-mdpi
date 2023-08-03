9cebb93cf05059a823565bbe6be0abcf94356149
Trevor Vannoy <trevorvannoy@montana.edu>
2023-08-03 11:22:40 -0600

    save row and image validation results

--- supervised-classification/training/hyperparameter-tuning/writeValidationResultsToTxtFile.m
+++ supervised-classification/training/hyperparameter-tuning/writeValidationResultsToTxtFile.m
@@ -1,31 +1,115 @@
-function writeValidationResultsToTxtFile(classifierName)
+function writeValidationResultsToTxtFile(classifierName,isRowMethod)
 
 arguments
     classifierName (1,1) string
+    isRowMethod (1,1) logical
 end
 
 % Setup data paths
 beehiveDataSetup;
 
+% Open output file
+fd = fopen(hyperparameterResultsDir + filesep + classifierName + "Results.txt", "w");
+
 % Load in the hyperparameter tuning results
 load(hyperparameterResultsDir + filesep + classifierName + "Hyperparams.mat","results");
 
-% Find the results with the minimum objective
-minIdx = results.IndexOfMinimumTrace(end);
-classificationResults = results.UserDataTrace{minIdx};
 
-% Write the classification results to the output text file
-fd = fopen(hyperparameterResultsDir + filesep + classifierName + "Results.txt", "w");
 
-fprintf(fd,"Confusion\n");
-fprintf(fd,"\t%6u\t%6u\n",classificationResults.Confusion(1,1),classificationResults.Confusion(1,2));
-fprintf(fd,"\t%6u\t%6u\n",classificationResults.Confusion(2,1),classificationResults.Confusion(2,2));
+if isRowMethod
+    % Find the results with the minimum objective
+    minIdx = results.IndexOfMinimumTrace(end);
+    rowResults = results.UserDataTrace{minIdx};
 
-fprintf(fd,"Precision = %.3f\n",classificationResults.Precision);
-fprintf(fd,"Recall = %.3f\n",classificationResults.Recall);
-fprintf(fd,"F2 = %.3f\n",classificationResults.F2);
-fprintf(fd,"MCC = %.3f\n",classificationResults.MCC);
-fprintf(fd,"Accuracy = %.3f\n",classificationResults.Accuracy);
+    % Load in the sampling results (if it exists) to check if the default
+    % parameters performed better than the parameters tried by bayesopt
+    filepath = samplingResultsDir + filesep + classifierName + "BestParams.mat";
+    if exist(filepath,"file")
+
+        load(filepath,"classificationResults");
+        
+        if classificationResults.MCC > rowResults.MCC
+            rowResults = classificationResults;
+        end
+    end
+
+    % Load in the validation labels since we didn't save them in the
+    % results struct... 
+    %TODO: we might want to be consistent with the testing results and save
+    % the true validation labels in the validation results struct
+    load(validationDataDir + filesep + "validationData","validationRowLabels");
+
+    if contains(classifierName,"CNN1d")
+        trueLabels = DeepLearning1dClassifier.formatLabels(validationRowLabels);
+    else
+        trueLabels = StatsToolboxClassifier.formatLabels(validationRowLabels);
+    end
+
+    % Compute the image-based results
+    imageResults.Confusion = imageConfusion(rowResults.PredLabels,trueLabels);
+    
+    [a, p, r, f2, mcc] = analyzeConfusion(imageResults.Confusion);
+    imageResults.Accuracy = a;
+    imageResults.Precision = p;
+    imageResults.Recall = r;
+    imageResults.F2 = f2;
+    imageResults.MCC = mcc;
+
+    % Write the classification results to the output text file
+    fprintf(fd,"Row results:\n");
+
+    fprintf(fd,"Confusion\n");
+    fprintf(fd,"\t%6u\t%6u\n",rowResults.Confusion(1,1),rowResults.Confusion(1,2));
+    fprintf(fd,"\t%6u\t%6u\n",rowResults.Confusion(2,1),rowResults.Confusion(2,2));
+    
+    fprintf(fd,"Precision = %.3f\n",rowResults.Precision);
+    fprintf(fd,"Recall = %.3f\n",rowResults.Recall);
+    fprintf(fd,"F2 = %.3f\n",rowResults.F2);
+    fprintf(fd,"MCC = %.3f\n",rowResults.MCC);
+    fprintf(fd,"Accuracy = %.3f\n",rowResults.Accuracy);
+
+    fprintf(fd,"\n\n");
+
+    fprintf(fd,"Image results:\n");
+
+    fprintf(fd,"Confusion\n");
+    fprintf(fd,"\t%6u\t%6u\n",imageResults.Confusion(1,1),imageResults.Confusion(1,2));
+    fprintf(fd,"\t%6u\t%6u\n",imageResults.Confusion(2,1),imageResults.Confusion(2,2));
+    
+    fprintf(fd,"Precision = %.3f\n",imageResults.Precision);
+    fprintf(fd,"Recall = %.3f\n",imageResults.Recall);
+    fprintf(fd,"F2 = %.3f\n",imageResults.F2);
+    fprintf(fd,"MCC = %.3f\n",imageResults.MCC);
+    fprintf(fd,"Accuracy = %.3f\n",imageResults.Accuracy);
+    
+else
+    % Find the results with the minimum objective
+    minIdx = results.IndexOfMinimumTrace(end);
+    imageResults = results.UserDataTrace{minIdx};
+
+    % Load in the default training results to check if the default
+    % parameters performed better than the parameters tried by bayesopt
+    filepath = default2dCNNResultsDir + filesep + classifierName + "ManualParamTraining.mat";
+
+    load(filepath,"userdata");
+    
+    if userdata.MCC > imageResults.MCC
+        imageResults = userdata;
+    end
+    
+    % Write the classification results to the output text file
+    fprintf(fd,"Image results:\n");
+
+    fprintf(fd,"Confusion\n");
+    fprintf(fd,"\t%6u\t%6u\n",imageResults.Confusion(1,1),imageResults.Confusion(1,2));
+    fprintf(fd,"\t%6u\t%6u\n",imageResults.Confusion(2,1),imageResults.Confusion(2,2));
+    
+    fprintf(fd,"Precision = %.3f\n",imageResults.Precision);
+    fprintf(fd,"Recall = %.3f\n",imageResults.Recall);
+    fprintf(fd,"F2 = %.3f\n",imageResults.F2);
+    fprintf(fd,"MCC = %.3f\n",imageResults.MCC);
+    fprintf(fd,"Accuracy = %.3f\n",imageResults.Accuracy);
+end
 
 fclose(fd);
 
