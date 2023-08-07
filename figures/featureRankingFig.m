e202e29edfad3e4a9dfffb87eec44ccd5cb39da6
Trevor Vannoy <trevorvannoy@montana.edu>
2023-08-07 15:11:50 -0600

    use mutual information for feature ranking

--- figures/featureRankingFig.m
+++ figures/featureRankingFig.m
@@ -5,26 +5,34 @@ beehiveDataSetup;
 load(trainingDataDir + filesep + "trainingFeatures","trainingFeatures");
 load(trainingDataDir + filesep + "trainingData","trainingRowLabels");
 
+mutualInfo = readmatrix(baseResultsDir + filesep + "feature-analysis" + filesep + "mi.csv");
 
 features = vertcat(trainingFeatures{:});
 labels = vertcat(trainingRowLabels{:});
 
+featureNames = string(features.Properties.VariableNames);
+
+%%
+% close all;
+% [idx, scores] = fscmrmr(features, labels);
+
 %%
 close all;
-[idx, scores] = fscmrmr(features, labels);
-
-%%
-fontSize = 10;
+fontSize = 8;
 fontName = "Tex Gyre Pagella";
 
-colors = colororder(brewermap([],'dark2'));
+colors = colororder(brewermap([],'paired'));
+darkBlue = colors(2,:);
 
 fig = figure('Units', 'centimeter', 'Position', [2 2 18.46 20]);
 
-bar(scores(idx),0.7,'FaceColor',colors(1,:),'Horizontal',true);
-xlabel('Importance')
-yticks(1:numel(idx))
-yticklabels(features.Properties.VariableNames(idx))
+sortedBar(mutualInfo,featureNames,FaceColor=darkBlue,...
+    FontName=fontName,LabelFontSize=fontSize,ShowValuesInBars=false);
+
+% bar(scores(idx),0.7,'FaceColor',colors(1,:),'Horizontal',true);
+xlabel('Mutual information')
+% yticks(1:numel(idx))
+% yticklabels(features.Properties.VariableNames(idx))
 % xtickangle(45)
 set(gca, 'FontSize', fontSize)
 set(gca, 'FontName', fontName)
