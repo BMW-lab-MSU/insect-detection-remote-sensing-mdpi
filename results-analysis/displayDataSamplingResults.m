84a90febe375fb2e8b2a3fb2451c6e06d71cf4dc
Trevor Vannoy <trevorvannoy@montana.edu>
2023-08-22 15:13:24 -0600

    add function to display grid search MCC performance

---
+++ results-analysis/displayDataSamplingResults.m
@@ -0,0 +1,29 @@
+function displayDataSamplingResults(classifierName)
+
+beehiveDataSetup;
+
+% Find all the data-sampling results files
+files = dir(samplingResultsDir + filesep + classifierName + "Undersample*.mat");
+filenames = string({files.name});
+
+% Load in the results
+for fileNum = 1:numel(filenames)
+    results(fileNum) = load(samplingResultsDir + filesep + filenames(fileNum));
+end
+
+% Display the results
+[results.objective]
+
+% Plot the results as a surface plot
+undersampleGrid = reshape([results.undersampleRatio],4,[]);
+oversampleGrid = reshape([results.nOversample],4,[]);;
+objective = reshape([results.objective],4,[]);
+
+
+surfc(undersampleGrid,oversampleGrid,objective);
+xlabel("undersampling")
+ylabel("oversampling")
+
+
+end
+
