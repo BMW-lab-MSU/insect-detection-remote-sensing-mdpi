0b2cd0868ae1ee0c94b43b6031e3ed06308c2949
Trevor Vannoy <trevorvannoy@montana.edu>
2023-11-08 14:04:11 -0700

    use a function to avoid workspace pollution

--- supervised-classification/training/data-sampling/createDataSamplingGrid.m
+++ supervised-classification/training/data-sampling/createDataSamplingGrid.m
@@ -1,4 +1,7 @@
 % SPDX-License-Identifier: BSD-3-Clause
+function createDataSamplingGrid
+
+beehiveDataSetup;
 
 % Setup undersampling and augmentation ranges
 params.UndersamplingRatio = [0 0.25 0.5 0.75];
@@ -7,3 +10,5 @@ params.NSyntheticInsect = round([0,logspace(0,log10(100),3)]);
 samplingGrid = formatGridSearchParams(params);
 
 save(trainingDataDir + filesep + "samplingGridRowBased", 'samplingGrid');
+
+end
