835115c7d8161ebc88f79c92fcd17a8176f24ff2
Trevor Vannoy <trevorvannoy@montana.edu>
2023-11-22 10:12:17 -0700

    include changepoint methods in analysis

--- results-analysis/analyzeConfidenceRatingEffects.m
+++ results-analysis/analyzeConfidenceRatingEffects.m
@@ -5,9 +5,18 @@ classifiers = ["RUSBoost","AdaBoost","StatsNeuralNetwork1Layer",...
     "StatsNeuralNetwork7Layer","CNN1d1Layer","CNN1d3Layer","CNN1d5Layer",...
     "CNN1d7Layer"];
 
-nClassifiers = numel(classifiers);
+changepointMethods = ["gfpopRows","matlabRows","gfpopCols","matlabCols",...
+    "gfpopBoth","matlabBoth"];
+changepointResultsFilename = ["rowResultsOriginal_gfpop",...
+    "rowResultsOriginal_matlab","colResultsOriginal_gfpop",...
+    "colResultsOriginal_matlab","bothResultsOriginal_gfpop",...
+    "bothResultsOriginal_matlab"];
 
-confidenceMetrics = dictionary(classifiers,repelem(struct(),1,nClassifiers));
+methodNames = [classifiers, changepointMethods];
+
+nMethods = numel(methodNames);
+
+confidenceMetrics = dictionary(methodNames,repelem(struct(),1,nMethods));
 
 load(testingDataDir + filesep + "testingData","testingRowLabels","testingRowConfidence")
 
@@ -20,6 +29,7 @@ confidenceLevels(confidenceLevels == 0) = [];
 beeIdx = find(trueLabels);
 
 for classifier = classifiers
+    disp(classifier)
     load(testingResultsDir + filesep + classifier + "Results.mat")
 
     predictions = results.Row.PredictedLabels;
@@ -41,15 +51,39 @@ for classifier = classifiers
         metrics(confidenceLevel).PctCorrect = metrics(confidenceLevel).NumCorrect / numel(idx);
         metrics(confidenceLevel).NumBees = numel(idx);
 
-        confidenceMetrics(classifier).Metrics = metrics;
+        confidenceMetrics(classifier).Confidence = metrics;
+    end
+end
+
+for i = 1:numel(changepointMethods)
+    disp(changepointMethods(i))
+    load(changepointResultsDir + filesep + changepointResultsFilename(i))
+
+    predictions = logical(horzcat(results{2}{:})');
+
+    wasPredictionCorrect = predictions == trueLabels;
+
+    wasBeePredictionCorrect = wasPredictionCorrect(beeIdx);
+
+    beeConfidenceRatings = confidenceRatings(beeIdx);
+
+    for confidenceLevel = confidenceLevels'
+        idx = find(beeConfidenceRatings == confidenceLevel);
+
+        metrics(confidenceLevel).NumCorrect = sum(wasBeePredictionCorrect(idx) == true);
+        metrics(confidenceLevel).NumIncorrect = sum(wasBeePredictionCorrect(idx) == false);
+        metrics(confidenceLevel).PctCorrect = metrics(confidenceLevel).NumCorrect / numel(idx);
+        metrics(confidenceLevel).NumBees = numel(idx);
+
+        confidenceMetrics(changepointMethods(i)).Confidence = metrics;
     end
 end
 
 save(testingResultsDir + filesep + "confidenceRatingEffects.mat","confidenceMetrics")
 
-pctCorrect = zeros(numel(classifiers),numel(confidenceLevels));
-for i = 1:numel(classifiers)
-    pctCorrect(i,:) = [confidenceMetrics(classifiers(i)).Metrics.PctCorrect];
+pctCorrect = zeros(nMethods,numel(confidenceLevels));
+for i = 1:nMethods
+    pctCorrect(i,:) = [confidenceMetrics(methodNames(i)).Confidence.PctCorrect];
 end
 
-bar(categorical(classifiers),pctCorrect)
+bar(categorical(methodNames),pctCorrect)
