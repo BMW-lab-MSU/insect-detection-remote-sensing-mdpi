0ba7263681e55095996638b3980b2c370dcdcdc9
Trevor Vannoy <trevorvannoy@montana.edu>
2023-12-07 13:19:36 -0700

    remove unused file

--- changepoint-detection/analyzeResults.m
+++
@@ -1,8 +0,0 @@
-% SPDX-License-Identifier: BSD-3-Clause
-function [confMat,accuracy,recall] = analyzeResults(predicted,true)
-
-    confMat = confusionmat(true,predicted);
-    accuracy = 0;
-    recall = 0;
-
-end
